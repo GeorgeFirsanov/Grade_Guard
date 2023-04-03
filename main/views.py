c7aad6960a5678bd2a15c10de9445dceb54ea658
George Firsanov <georgfirsanov@gmail.com>
2023-04-03 11:04:34 +0300

    change folder name to 'front' and put it in 'templates'

--- main/views.py
+++ main/views.py
@@ -1,13 +1,12 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
-
 def index(request):
-    return render(request, 'main/index.html')
+    return render(request, 'front/index.html')
 
 def teacher(request):
     return render(request, 'main/teacher.html')
 
 def student(request):
-    return render(request, 'main/student.html')
+    return render(request, 'front/subjects.html')
 
