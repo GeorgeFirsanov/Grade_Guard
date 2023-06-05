2a0bd346d8feb5ea4d3c045428118370cd6b6a51
Bachateros <ser-morkovka47@mail.ru>
2023-06-06 00:45:23 +0300

    fix main page

--- main/static/js/editTable.js
+++ main/static/js/editTable.js
@@ -2,4 +2,6 @@ const postButton = document.querySelector('.form__save');
 const form = document.querySelector('.content__form');
 const table = document.querySelector('.form__table');
 
-
+postButton.addEventListener('click', ()=>{
+    
+})
