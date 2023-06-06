a0e51fccb181b8385308f2a90612329b2f68d1db
Bachateros <ser-morkovka47@mail.ru>
2023-06-06 04:03:05 +0300

    standartization footer and connect js-file

--- main/static/js/editTable.js
+++ main/static/js/editTable.js
@@ -3,5 +3,8 @@ const form = document.querySelector('.content__form');
 const table = document.querySelector('.form__table');
 
 postButton.addEventListener('click', ()=>{
-    
+    console.log('test');
 })
+setTimeout(function(){
+    window.location.reload(1);
+ }, 4000);
